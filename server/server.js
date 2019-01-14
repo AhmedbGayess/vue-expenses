a28aae262d0d8d49832bc119e1bd3d453b26550d
AhmedbGayess <38194881+AhmedbGayess@users.noreply.github.com>
2019-01-14 17:37:39 +0100

    deployment config

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 const cors = require("cors");
+const serveStatic = require("serve-static");
 
 const users = require("./routes/users");
 const expenses = require("./routes/expenses");
@@ -35,14 +36,11 @@ require("./config/passport")(passport);
 app.use("/api/users", users);
 app.use("/api/expenses", expenses);
 
-// Handle production
-if (process.env.NODE_ENV === "production") {
-    // Static folder
-    app.use(express.static(__dirname + "/public/"));
+// Static folder
+app.use(serveStatic(__dirname + "/public"));
 
-    //Handle single page application
-    app.get(/.*/, (req, res) => res.sendFile(__dirname + "/public/index.html"));
-}
+//Handle single page application
+app.get(/.*/, (req, res) => res.sendFile(__dirname + "/public/index.html"));
 
 const port = process.env.PORT || 5000;
 
